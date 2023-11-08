8425ceda7034079df3eb3dbd70bee369d1cefccb
Vlad <vmusaelyae@gmail.com>
2023-11-08 14:26:30 +0200

    added app main_chat

--- chatSynthia/settings.py
+++ chatSynthia/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'main_chat',
 ]
 
 MIDDLEWARE = [
---
+++ main_chat/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MainChatConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'main_chat'
