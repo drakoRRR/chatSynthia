5dddb0a22e951fd1f943810f76e4ef3dfa130ccf
Vlad <vmusaelyae@gmail.com>
2023-11-08 14:31:58 +0200

    added main template chat, and static

--- chatSynthia/settings.py
+++ chatSynthia/settings.py
@@ -127,7 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static/']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
